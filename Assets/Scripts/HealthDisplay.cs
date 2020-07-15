e90b819c0d08d65984b2b6ff2685e5794eeb3c2d
Michał Czekański <michal.cz2202@gmail.com>
2020-07-15 12:43:07 +0200

    Add script displaying hp

---
+++ Assets/Scripts/HealthDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour
+{
+    private const string displayString = "Health: {0}";
+    public Text textDisplay;
+    public PlayerController playerController;
+
+    private int playerHp;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        playerHp = playerController.Hp;
+        textDisplay.text = string.Format(displayString, playerHp);
+    }
+}
