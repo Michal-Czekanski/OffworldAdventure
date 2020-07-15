3ce2c80ebc95044e6e0c4bb92de63a0d87e4006f
Michał Czekański <michal.cz2202@gmail.com>
2020-07-15 13:04:36 +0200

    Add points displaying script

---
+++ Assets/Scripts/PointsDisplay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointsDisplay : MonoBehaviour
+{
+    private const string displayString = "Points: {0}";
+    public Text textDisplay;
+    public PlayerController playerController;
+
+    private int playerPoints;
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPoints = playerController.Points;
+        textDisplay.text = string.Format(displayString, playerPoints);
+    }
+}
