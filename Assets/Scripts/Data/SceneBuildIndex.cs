e0580cb65c5f43d555354e3c04db2ea2be283b6a
miccze <miccze7@st.amu.edu.pl>
2020-12-02 21:21:58 +0100

    Add SceneBuildIndexEnum

---
+++ Assets/Scripts/Data/SceneBuildIndex.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Data
+{
+    public enum SceneBuildIndex
+    {
+        MainMenu = 0, SelectLevel = 1
+    }
+}
