9384698a11cfb0cee5fde6c35324ccb9a0d1cb4a
miccze <miccze7@st.amu.edu.pl>
2020-12-02 21:09:44 +0100

    Add opening SelectLevel scn on SelectLevel btn clk

--- Assets/Scripts/MainMenuButtonHandler.cs
+++ Assets/Scripts/MainMenuButtonHandler.cs
@@ -1,17 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// This class is used to handle button clicks in MainMenu scene
 /// </summary>
 public class MainMenuButtonHandler : MonoBehaviour
 {
+    public static int selectLevelSceneIndex = 1;
     /// <summary>
     /// This method is called when Quit button is clicked
     /// </summary>
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    /// <summary>
+    /// This method is called when SelectLevel button is clicked.
+    /// It opens SelectLevel screen.
+    /// </summary>
+    public void SelectLevelClick()
+    {
+        SceneManager.LoadScene(selectLevelSceneIndex);
     }
 }
