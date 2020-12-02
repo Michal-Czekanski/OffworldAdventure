09a93b89da7b998a9b5572dc9d45b7210e428a95
miccze <miccze7@st.amu.edu.pl>
2020-12-02 21:02:39 +0100

    Comment MainMenuButtonHandler script

--- Assets/Scripts/MainMenuButtonHandler.cs
+++ Assets/Scripts/MainMenuButtonHandler.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This class is used to handle button clicks in MainMenu scene
+/// </summary>
 public class MainMenuButtonHandler : MonoBehaviour
 {
+    /// <summary>
+    /// This method is called when Quit button is clicked
+    /// </summary>
     public void QuitGame()
     {
         Application.Quit();
