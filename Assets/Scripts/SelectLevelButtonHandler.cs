86150f187f72be520eb2403fdb50be5a18d16da2
miccze <miccze7@st.amu.edu.pl>
2020-12-02 21:23:23 +0100

    Comment SelectLevelButtonHandler class

--- Assets/Scripts/SelectLevelButtonHandler.cs
+++ Assets/Scripts/SelectLevelButtonHandler.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This class is used to handle button clicks in SelectLevel scene
+/// </summary>
 public class SelectLevelButtonHandler : MonoBehaviour
 {
     // Start is called before the first frame update
