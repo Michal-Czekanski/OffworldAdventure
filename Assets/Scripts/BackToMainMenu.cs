fc0a36be5218f25553ddfb1d69abdc35b47fb5a9
Michael-Czekanski <michal.cz2202@gmail.com>
2020-12-09 00:02:16 +0100

    Add returning to MainMenu from Options

---
+++ Assets/Scripts/BackToMainMenu.cs
@@ -0,0 +1,16 @@
+﻿using Assets.Scripts.Data;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMainMenu : MonoBehaviour
+{
+    /// <summary>
+    /// This method is called when user wants to return to MainMenu.
+    /// </summary>
+    public void Back()
+    {
+        SceneManager.LoadScene((int)SceneBuildIndex.MainMenu);
+    }
+}
