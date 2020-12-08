f59f11f687324874b198472b9584fefd6eaab48e
Michael-Czekanski <michal.cz2202@gmail.com>
2020-12-08 14:25:18 +0100

    Add SelectLevelButtonController

---
+++ Assets/Scripts/SelectLevelScene/SelectLevelButtonController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Each LevelButton in SelectLevel scene should have this script attached.
+/// </summary>
+public class SelectLevelButtonController : MonoBehaviour
+{
+    public int levelNum;
+}
