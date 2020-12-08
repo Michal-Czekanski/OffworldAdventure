5aabbff273f294cfd56593323fc1ac8ac66a8b5a
Michael-Czekanski <michal.cz2202@gmail.com>
2020-12-08 14:35:42 +0100

    Add LoadLevel method in LevelManager cls

--- Assets/Scripts/Utils/LevelManager.cs
+++ Assets/Scripts/Utils/LevelManager.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using Assets.Scripts.Data;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// This class is used to manage levels
@@ -20,5 +22,19 @@ public static class LevelManager
     public static bool IsLevelUnlocked(int levelNum)
     {
         return levelNum <= PlayerPrefs.GetInt(lastUnlockedLevelPrefsString, 1);
+    }
+
+    /// <summary>
+    /// Loads level if it is unlocked.
+    /// </summary>
+    /// <param name="levelNum">Level number to load.</param>
+    /// <returns>True if level was loaded, false otherwise.</returns>
+    public static bool LoadLevel(int levelNum)
+    {
+        if (IsLevelUnlocked(levelNum))
+        {
+            SceneManager.LoadScene((int)SceneBuildIndex.Level1 + levelNum - 1);
+        }
+        return false;
     }
 }
