642df92f040e5142c8755416fc8f9077675ab852
Michael-Czekanski <michal.cz2202@gmail.com>
2020-12-17 21:32:10 +0100

    Add empty OptionsManager cls

---
+++ Assets/Scripts/Utils/OptionsManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// <para> Class used to manage game options.</para>
+/// Possible options:
+/// <list type="bullet">
+/// <item>Resolution</item>
+/// <item>Fullscreen</item>
+/// <item>Sound</item>
+/// </list>
+///
+/// </summary>
+public static class OptionsManager
+{
+    
+}
