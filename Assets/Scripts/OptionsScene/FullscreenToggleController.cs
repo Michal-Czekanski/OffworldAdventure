e455ad69c5e2c7f3dfdf5d65323aff0f611636db
Michael-Czekanski <michal.cz2202@gmail.com>
2020-12-28 14:44:25 +0100

    Add updating FullscreenToggle state on scene start

--- Assets/Scripts/OptionsScene/FullscreenToggleController.cs
+++ Assets/Scripts/OptionsScene/FullscreenToggleController.cs
@@ -1,14 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// Manages FullscreenToggle which is in OptionsScene.
 /// </summary>
 public class FullscreenToggleController : MonoBehaviour
 {
+    private Toggle toggle;
+
     /// <summary>
-    /// 
+    /// On start switch toggle off if fullscreen is disabled.
+    /// </summary>
+    private void Start()
+    {
+        toggle = GetComponent<Toggle>();
+        toggle.isOn = OptionsManager.GetFullscreen();
+    }
+
+    /// <summary>
     /// <para>This method is called when fullscreen toggle value is changed.</para>
     /// <para>Sets fullscreen using <see cref="OptionsManager"/>.</para>
     /// </summary>
