53d01c9b912677b3788d1afd5052699835a3ef40
Michael-Czekanski <michal.cz2202@gmail.com>
2020-12-28 16:28:38 +0100

    Add filling ResolutionDropdown with resolutions

--- Assets/Scripts/OptionsScene/ResolutionDropdownController.cs
+++ Assets/Scripts/OptionsScene/ResolutionDropdownController.cs
@@ -1,11 +1,34 @@
-﻿using System.Collections;
+﻿using Assets.Scripts.Data;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// Manages ResolutionDropdown which is in OptionsScene.
 /// </summary>
 public class ResolutionDropdownController : MonoBehaviour
 {
+    /// <summary>
+    /// ResolutionDropdown
+    /// </summary>
+    TMP_Dropdown dropdown;
+    private void Start()
+    {
+        dropdown = GetComponent<TMP_Dropdown>();
+        FillDropdownWithResolutions();
+    }
 
+    /// <summary>
+    /// Fills ResolutionDropdown with possible resolutions to choose.
+    /// </summary>
+    private void FillDropdownWithResolutions()
+    {
+        dropdown.options.Clear();
+        foreach (Assets.Scripts.Data.Resolution resolution in Resolutions.allResolutions)
+        {
+            dropdown.options.Add(new TMP_Dropdown.OptionData(resolution.AsString));
+        }
+    }
 }
