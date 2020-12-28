2a876aa4e5d6431a4c4c1d9c84c1c68a8e0f9e0f
Michael-Czekanski <michal.cz2202@gmail.com>
2020-12-28 14:02:50 +0100

    Add setting fullscreen in OptionsScene

--- Assets/Scripts/OptionsScene/FullscreenToggleController.cs
+++ Assets/Scripts/OptionsScene/FullscreenToggleController.cs
@@ -7,5 +7,14 @@ using UnityEngine;
 /// </summary>
 public class FullscreenToggleController : MonoBehaviour
 {
-    
+    /// <summary>
+    /// 
+    /// <para>This method is called when fullscreen toggle value is changed.</para>
+    /// <para>Sets fullscreen using <see cref="OptionsManager"/>.</para>
+    /// </summary>
+    /// <param name="fullscreen">Boolean value which tells if fullscreen should be enabled or disabled.</param>
+    public void ValueChanged(bool fullscreen)
+    {
+        OptionsManager.SetFullscreen(fullscreen);
+    }
 }
