d7a6ec33bfe880dfb78137e95957ac980331cc78
Michael-Czekanski <michal.cz2202@gmail.com>
2020-12-28 14:47:04 +0100

    Add empty ResolutionDropdownController cls

---
+++ Assets/Scripts/OptionsScene/ResolutionDropdownController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Manages ResolutionDropdown which is in OptionsScene.
+/// </summary>
+public class ResolutionDropdownController : MonoBehaviour
+{
+
+}
