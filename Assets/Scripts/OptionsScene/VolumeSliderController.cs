b234e7d0cd5a9d405340b28d33d324eb36a49c69
Michael-Czekanski <michal.cz2202@gmail.com>
2020-12-30 11:26:56 +0100

    Add empty VolumeSliderController cls

---
+++ Assets/Scripts/OptionsScene/VolumeSliderController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Manages VolumeSlider which is in Options scene.
+/// </summary>
+public class VolumeSliderController : MonoBehaviour
+{
+}
