c43e51d077fce550628cea8f91c74286e4dd39e7
Michael-Czekanski <michal.cz2202@gmail.com>
2020-12-30 16:40:41 +0100

    Fix trying to access null field on btn disable
    In highlight script.

--- Assets/Scripts/ButtonHighlight.cs
+++ Assets/Scripts/ButtonHighlight.cs
@@ -70,6 +70,10 @@ public class ButtonHighlight : MonoBehaviour, IPointerEnterHandler, IPointerExit
     /// </summary>
     private void OnDisable()
     {
-        StopHighlight();
+        if(textView != null)
+        {
+            StopHighlight();
+
+        }
     }
 }
