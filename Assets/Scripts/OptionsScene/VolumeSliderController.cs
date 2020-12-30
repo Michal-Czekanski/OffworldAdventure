d2a66244523c291fac91db9982b4832a425a1159
Michael-Czekanski <michal.cz2202@gmail.com>
2020-12-30 11:31:35 +0100

    Add setting volume slider val on scn start

--- Assets/Scripts/OptionsScene/VolumeSliderController.cs
+++ Assets/Scripts/OptionsScene/VolumeSliderController.cs
@@ -1,10 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// Manages VolumeSlider which is in Options scene.
 /// </summary>
 public class VolumeSliderController : MonoBehaviour
 {
+    private Slider volumeSlider;
+
+    /// <summary>
+    /// On awake set VolumeSlider's value to be the same as in the preferences. 
+    /// </summary>
+    private void Awake()
+    {
+        volumeSlider = GetComponent<Slider>();
+        volumeSlider.value = OptionsManager.GetSoundLevel().Level;
+    }
 }
